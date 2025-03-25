3c3f6bdc53dd47d9ef7dc6d08ceab488fe3a6073
Brandon Robinson <brantylrobinson@gmail.com>
2025-03-25 16:35:55 -0400

    add if else statement for ban thresholds and warning message

--- telegram.py
+++ telegram.py
@@ -1,13 +1,19 @@
 import os
+import time
 import telebot
 from dotenv import load_dotenv
-from app.py import analyze_text
+from app import analyze_text
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("API_KEY")
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+user_warning={}
+ban_threshold=3
+
+curtime = time.ctime(1627908313.717886)
 
 
 # Handles /start command and sends a welcome message
@@ -18,8 +24,18 @@ def send_welcome(message):
 # analyze and respond to all incoming messages through telegram
 @bot.message_handler(func=lambda msg: True)
 def analyze_and_respond(message):
+    user_id = message.from_user.id
     analysis_result = analyze_text(message.text)
-    bot.reply_to(message, analysis_result)
+   
+    if "🚫 Hate Speech Detected" in analysis_result:
+        user_warning[user_id] = user_warning.get(user_id, 0) + 1
+
+        if user_warning[user_id] >= ban_threshold:
+            bot.reply_to(message, "user has been banned contents logged")
+            bot.reply_to(message, f"At {curtime} User: {message.from_user.first_name} has been warned over {ban_threshold} to stop spreading vulgar language and hate speach")
+            return
+
+    bot.reply_to(message, analysis_result)            
 
 # Keep the bot running
 bot.infinity_polling()
