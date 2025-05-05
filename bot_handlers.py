3f6cff559c5af0ffe5bf9382bf4e28058a607d55
Brandon Robinson <brantylrobinson@gmail.com>
2025-05-05 16:45:57 -0400

    yapf format

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,9 +1,9 @@
 from config import bot
-from bot_commands import (
-    send_welcome, set_flagged_word, delete_rule, set_rule,
-    list_banned_users, list_rules, set_ban_lenth,
-    set_warning_threshold, analyze_and_respond
-)
+from bot_commands import (send_welcome, set_flagged_word, delete_rule,
+                          set_rule, list_banned_users, list_rules,
+                          set_ban_length, chatique_help, set_warning_threshold,
+                          analyze_and_respond)
+
 
 def register_handlers():
     bot.message_handler(commands=['start'])(send_welcome)
@@ -12,6 +12,8 @@ def register_handlers():
     bot.message_handler(commands=['sr', 'setrulebook'])(set_rule)
     bot.message_handler(commands=['lb', 'listban'])(list_banned_users)
     bot.message_handler(commands=['r', 'rules'])(list_rules)
-    bot.message_handler(commands=['setban', 'sb'])(set_ban_lenth)
+    bot.message_handler(commands=['setban', 'sb'])(set_ban_length)
     bot.message_handler(commands=['sw', 'setwarning'])(set_warning_threshold)
+    bot.message_handler(commands=['ch', 'help'])(chatique_help)
+    """Roberta Model And Text Detection"""
     bot.message_handler(func=lambda msg: True)(analyze_and_respond)
