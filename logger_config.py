d1c62faa51c64050f8ec0c23cf1851754aca5716
Brandon Robinson <brantylrobinson@gmail.com>
2025-05-05 16:46:08 -0400

    yapf format

--- logger_config.py
+++ logger_config.py
@@ -2,7 +2,7 @@ import logging
 
 # Logger Module
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)  
+logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 
