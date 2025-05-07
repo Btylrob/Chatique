c515d43ce7ea4ac8a1a8217992821d54aaf08c35
Brandon Robinson <brantylrobinson@gmail.com>
2025-05-07 12:55:00 -0400

    add turn off url handler

--- bot_handlers.py
+++ bot_handlers.py
@@ -2,7 +2,7 @@ from config import bot
 from bot_commands import (send_welcome, set_flagged_word, delete_rule,
                           set_rule, list_banned_users, list_rules,
                           set_ban_length, chatique_help, set_warning_threshold,
-                          analyze_and_respond)
+                          analyze_and_respond, turn_off_url)
 
 
 def register_handlers():
@@ -15,5 +15,6 @@ def register_handlers():
     bot.message_handler(commands=['setban', 'sb'])(set_ban_length)
     bot.message_handler(commands=['sw', 'setwarning'])(set_warning_threshold)
     bot.message_handler(commands=['ch', 'help'])(chatique_help)
+    bot.message_handler(commands=['toggle_url', 't-url'])(turn_off_url)
     """Roberta Model And Text Detection"""
     bot.message_handler(func=lambda msg: True)(analyze_and_respond)
